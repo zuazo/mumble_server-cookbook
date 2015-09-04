4f9713564eecabce4381a14f9eaec7fa9d340781
Xabier de Zuazo <xabier@zuazo.org>
2015-09-04 09:15:09 +0200

    Add mumble_server_supw ChefSpec locator

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,5 +1,14 @@
 if defined?(ChefSpec)
 
+  if ChefSpec.respond_to?(:define_matcher)
+    # ChefSpec >= 4.1
+    ChefSpec.define_matcher :mumble_server_supw
+  elsif defined?(ChefSpec::Runner) &&
+        ChefSpec::Runner.respond_to?(:define_runner_method)
+    # ChefSpec < 4.1
+    ChefSpec::Runner.define_runner_method :mumble_server_supw
+  end
+
   def change_mumble_server_supw(password)
     ChefSpec::Matchers::ResourceMatcher.new(
       :mumble_server_supw, :change, password
